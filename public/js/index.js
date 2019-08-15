52e2cbc27eae2735028a3eeafda64bb72d2e0dc2
Kunal Aildasani <kunalaildasani@Kunals-MacBook-Pro.local>
2019-08-14 22:08:02 -0700

    Added bouncy effect to sidebar

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,22 @@
+$(document).ready(function()
+{	
+	window.onscroll = function() {applyNavbarPosition()};
+})
 
-console.log("hellooo")
+
+function applyNavbarPosition() {
+	let navbar = document.getElementById("navbar");
+	let overlay = document.getElementById("overlay");
+
+	let sticky = overlay.offsetTop + 48;
+
+	console.log(sticky)
+
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+		console.log("Sticky");
+	} else {
+		navbar.classList.remove("sticky");
+		console.log("Not Sticky");
+	}
+}
